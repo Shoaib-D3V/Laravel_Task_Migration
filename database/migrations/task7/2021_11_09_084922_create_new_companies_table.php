79d10c3a731e06e2d8f862d76368747a57a64d6d
Shoaib Qureshi <shoaib@d3vtech.com>
2024-11-14 13:24:33 +0530

    Update 2021_11_09_084922_create_new_companies_table.php

--- database/migrations/task7/2021_11_09_084922_create_new_companies_table.php
+++ database/migrations/task7/2021_11_09_084922_create_new_companies_table.php
@@ -18,6 +18,7 @@ class CreateNewCompaniesTable extends Migration
         Schema::create('companies', function (Blueprint $table) {
             $table->id();
             $table->string('name');
+            $table->string('name')->default('My company');
             $table->timestamps();
         });
     }
